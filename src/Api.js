c3100010f5616e3ca5cd0f804bf8d72bb391c70f
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2019-06-25 00:18:32 +0300

    Popping up ICONex and sort of preparing transaction

---
+++ src/Api.js
@@ -0,0 +1,69 @@
+import IconService from 'icon-sdk-js';
+
+
+class Api {
+    // ICONex Connect Extension =============================================================
+    iconexAskAddress () {
+        return this.__iconexConnectRequest('REQUEST_ADDRESS').then(payload => {
+            return payload;
+        })
+    }
+    // ======================================================================================
+
+    // Following classes are private because they are lower level methods at a protocol level
+    __sendTransaction (from, to, value, data) {
+        const transaction = this.__icxTransactionBuild(from, to, value, 2000000, data)
+        console.log(transaction)
+
+        const jsonRpcQuery = {
+            'jsonrpc': '2.0',
+            'method': 'icx_sendTransaction',
+            'params': IconService.IconConverter.toRawTransaction(transaction),
+            'id': 1234
+        };
+
+        return this.__iconexJsonRpc(jsonRpcQuery)
+    }
+
+    __iconexConnectRequest (requestType, payload) {
+        return new Promise((resolve, reject) => {
+            function eventHandler (event) {
+                const { payload } = event.detail;
+                window.removeEventListener('ICONEX_RELAY_RESPONSE', eventHandler)
+                resolve(payload)
+            }
+            window.addEventListener('ICONEX_RELAY_RESPONSE', eventHandler)
+
+            window.dispatchEvent(new window.CustomEvent('ICONEX_RELAY_REQUEST', {
+                detail: {
+                    type: requestType,
+                    payload
+                }
+            }))
+        })
+    }
+
+    __iconexJsonRpc (jsonRpcQuery) {
+        return this.__iconexConnectRequest('REQUEST_JSON-RPC', jsonRpcQuery).then(payload => {
+            return payload
+        })
+    }
+
+    __icxTransactionBuild (from, to, value, stepLimit, data) {
+        return new IconService.IconBuilder.MessageTransactionBuilder()
+            .from(from)
+            .to(to)
+            .value(IconService.IconAmount.of(value, IconService.IconAmount.Unit.ICX).toLoop())
+            .stepLimit(IconService.IconConverter.toBigNumber(stepLimit))
+            .nid(IconService.IconConverter.toBigNumber('0x3'))
+            .version(IconService.IconConverter.toBigNumber('0x3'))
+            .timestamp((new Date()).getTime() * 1000)
+            .data(data)
+            .build()
+    }
+
+
+
+}
+
+export  default Api;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Resizer from 'react-image-file-resizer';
+import Api from './Api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedFile: null,
+            from: null,
+            test: null
+        };
+
+        this.fileSelectedHandler = this.fileSelectedHandler.bind(this);
+        this.fileUploadHandler = this.fileUploadHandler.bind(this);
+    }
+
+
+    fileSelectedHandler = event => {
+        this.setState({
+            selectedFile: event.target.files[0]
+        })
+    };
+
+    fileUploadHandler = () => {
+        //Upload image to ICON blockchain
+        let api = new Api();
+        Resizer.imageFileResizer(
+            this.state.selectedFile,
+            1000,
+            1000,
+            'JPEG',
+            70,
+            0,
+            (image) => {
+                api.iconexAskAddress().then((address) => {
+                    //Get address from callback
+                    this.setState({from: address}, ()=>{
+                        api.__sendTransaction(this.state.from, 'hx0000000000000000000000000000000000000000', 0, image)
+                    });
+                });
+            },
+            'base64'
+        );
+
+    };
+
+    render() {
+        console.log(this.state.from);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <input type="file" onChange={this.fileSelectedHandler}/>
+                    <button value='Upload' onClick={this.fileUploadHandler}>Upload</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
